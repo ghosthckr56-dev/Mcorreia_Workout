69cecf5ed3ce9442542f70479c47bc808a9a7695
ghosthckr56-dev <ghosthckr56@gmail.com>
2026-08-16 20:58:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mcorreia-workout-v2";
+const CACHE = "mcorreia-workout-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", ""];
 
 self.addEventListener("install", (e) => {
